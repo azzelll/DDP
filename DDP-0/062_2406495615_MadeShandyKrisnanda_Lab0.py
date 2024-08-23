04e440e78a8cd8579843dc6e05441350f6b39582
azzelll <madeshandykrisnanda@gmail.com>
2024-08-23 08:54:00 +0700

    TEST

---
+++ DDP-0/062_2406495615_MadeShandyKrisnanda_Lab0.py
@@ -0,0 +1,34 @@
+import math
+print(">>====================================<<")
+print("||                                    ||")
+print("||  Welcome to Dek Depe's Calculator  ||")
+print("||                                    ||")
+print(">>====================================<<")
+print("")
+
+
+nama = input("Nama: ")
+tanggalLahir = input("Tanggal lahir: ")
+jurusan = input("Jurusan: ")
+kelompokMentoring = int(input("Kelompok mentoring: "))
+motto = input("Motto: ")
+alas = int(input("Masukan alas (cm): "))
+tinggi = int(input("Masukan tinggi (cm): "))
+print("")
+
+luas =  0.5 * alas * tinggi
+miring = math.sqrt((math.pow(alas,2))+(math.pow(tinggi,2)))
+keliling = math.ceil(alas + tinggi + miring)
+
+print('Halo {} dari jurusan {}.'.format(nama, jurusan))
+print('{} berasal dari kelompok mentoring {}.'.format(nama, kelompokMentoring))
+print('{} lahir pada {} dengan motto "{}"'.format(nama, tanggalLahir, motto))
+print('Luas dari segitiga yang dimiliki {} adalah {} cm^2'.format(nama, luas))
+print('Keliling dari segitiga yang dimiliki {} adalah {} cm dengan sisi miring sepanjang {}'.format(nama, keliling, miring))
+
+print("")
+print(">>==========================================<<")
+print("||                                          ||")
+print("||  Thanks for using Dek Depe's Calculator  ||")
+print("||                                          ||")
+print(">>==========================================<<")
