2acac744a7785d828d8d1fbad016e4be33fcfde8
azzelll <madeshandykrisnanda@gmail.com>
2024-08-29 11:01:39 +0700

    Structure Update

--- DDP-1/062_2406495615_MadeShandyKrisnanda_Lab1.py
+++ DDP-1/062_2406495615_MadeShandyKrisnanda_Lab1.py
@@ -3,24 +3,26 @@ print("||                     	   ||")
 print("|| Welcome to Interrogation||")
 print("||                     	   ||")
 print(">>==========================<<")
-print("")
+print()
 
+no_answer = ('n', 'N', 'no', 'No', 'NO')
+yes_answer = ('y', 'Y', 'yes', 'Yes', 'YES')
+all_answer = ('y', 'Y', 'yes', 'Yes', 'YES', 'n', 'N', 'no', 'No', 'NO')
 jawaban_interogasi = input("Mulai Interogasi(Y/N)?")
-if jawaban_interogasi in ('n', 'N', 'no', 'No', 'NO'):
-    print('Interogasi telah selesai')
-    print('')
+
+while jawaban_interogasi not in all_answer:
+    print('\ninput tidak valid')
+    jawaban_interogasi = input("Mulai Interogasi(Y/N)?")
+if jawaban_interogasi in (no_answer):
+    print('Interogasi telah selesai\n')
     print('>>==========================<<')
     print('||                     	    ||')
     print('|| Ending the Interrogation ||')
     print('||                     	    ||')
     print('>>==========================<<')
-while jawaban_interogasi not in ('n', 'N', 'no', 'No', 'NO', 'y', 'Y', 'yes', 'Yes', 'YES'):
-    print('')
-    print('input tidak valid')
-    jawaban_interogasi = input("Mulai Interogasi(Y/N)?")
-while jawaban_interogasi in ('y', 'Y', 'yes', 'Yes', 'YES'):
-    print("Mari kita mulai interogasi")
-    print(' ')
+while jawaban_interogasi in yes_answer:
+    print('Mari kita mulai interogasi\n')
+    print()
     is_stop = False
     while is_stop == False:
         nama = input("Siapa namamu?")
@@ -30,75 +32,51 @@ while jawaban_interogasi in ('y', 'Y', 'yes', 'Yes', 'YES'):
             digit_npm+=1
         if npm.isdigit()!= True:
             print("NPM harus berupa angka!")
-            print('------------------------------')
-            print(' ')
+            print('------------------------------\n')
             continue
         elif int(digit_npm) != 10:
             print("NPM harus sepanjang 10 digits!")
-            print('------------------------------')
-            print(' ')
+            print('------------------------------\n')
             continue
-        elif npm.isdigit() == True and int(digit_npm) == 10:
-            motif = input("Apakah kamu punya motif (Y/N)?")
-            if motif in ('y', 'Y', 'yes', 'Yes', 'YES'):
-                jenis_motif = input("Apa motif kamu tadi?")
-                alibi = input("Apakah kamu punya alibi (Y/N)?")
-                if alibi in ('y', 'Y', 'yes', 'Yes', 'YES'):
-                    jenis_alibi = input("Apa alibi kamu?")
-                    print(' ')
-                    print('{} dengan NPM {} memiliki motif {} dan memiliki alibi {}'.format(nama,npm,jenis_motif, jenis_alibi))
-                    print('{} lumayan mencurigakan nih...'.format(nama))
-                    print('------------------------------')
-                    print(' ')
-                elif alibi in ('n', 'N', 'no', 'No', 'NO'):
-                    print(' ')
-                    print('{} dengan NPM {} memiliki motif {} tapi tidak memiliki alibi'.format(nama,npm, jenis_motif))
-                    print('Wah, {} mencurigakan nihh >:('.format(nama))
-                    print('------------------------------')
-                    print(' ')
-                else:
-                    print('seseorang harus punya alibi atau tidak sama sekali')
-                    print('------------------------------')
-                    print(' ')
-                    continue
-            elif motif in ('n', 'N', 'no', 'No', 'NO'):
-                motif = "tidak memiliki motif"
-                alibi = input("Apakah kamu punya alibi (Y/N)?")
-                if alibi in ('y', 'Y', 'yes', 'Yes', 'YES'):
-                    jenis_alibi = input("Apa alibi kamu?")
-                    print(' ')
-                    print('{} dengan NPM {} tidak memiliki motif dan memiliki alibi {}'.format(nama,npm, jenis_alibi))
-                    print('{} tidak terlalu mencurigakan sih'.format(nama))
-                    print('------------------------------')
-                    print(' ')
-                elif alibi in ('n', 'N', 'no', 'No', 'NO'):
-                    print(' ')
-                    print('{} dengan NPM {} tidak memiliki motif maupun alibi'.format(nama,npm))
-                    print('{} lumayan mencurigakan nih...'.format(nama))
-                    print('------------------------------')
-                    print(' ')
-                else:
-                    print('seseorang harus punya alibi atau tidak sama sekali')
-                    print('------------------------------')
-                    print(' ')
-                    continue
-            else:
-                print("seseorang harus punya motif atau tidak sama sekali")
-                print('------------------------------')
-                print(' ')
+        elif npm.isdigit() and int(digit_npm) == 10:
+            has_motive = False
+            has_alibi = False
+            ask_motive = input("Apakah kamu punya motif (Y/N)?")
+            if ask_motive in yes_answer:
+                has_motive = True
+                type_motive = input('motifnya apa?')
+            elif ask_motive not in no_answer:
+                print('seseorang bla bla')
+                print('------------------------------\n')
                 continue
+            ask_alibi = input('Apakah punya alibi?')
+            if ask_alibi in yes_answer:
+                has_alibi = True
+                type_alibi = input('alibinya apa?')
+            elif ask_alibi not in no_answer:
+                print('seseorang bla bla')
+                print('------------------------------\n')
+                continue
+            print()
+            end_print = f"{nama} dengan NPM {npm}{' tidak' if has_motive == False else ''} memiliki motif {type_motive if has_motive == True else ''}{'tidak' if has_alibi == False else ''} memiliki alibi {type_alibi if has_alibi == True else ''}"
+            print(end_print)
+            if (has_motive and has_alibi) or (has_motive == False and has_alibi == False) :
+                print(f'{nama} lumayan mencurigakan nih...')
+            elif has_motive == False and has_alibi:
+                print(f'{nama} tidak terlalu mencurigakan sih')
+            elif has_motive and has_alibi == False:
+                print(f'wah, {nama} mencurigakan nih >:(')
+            print('------------------------------\n')
             lanjut_interogasi = input("Lanjut interogasi (Y/N))?")
-            while lanjut_interogasi not in ('n', 'N', 'no', 'No', 'NO', 'y', 'Y', 'yes', 'Yes', 'YES'):
-                print('')
+            while lanjut_interogasi not in all_answer:
+                print()
                 print('input tidak valid')
-                jawaban_interogasi = input("Lanjut interogasi (Y/N))?")
-            if lanjut_interogasi in ('y', 'Y', 'yes', 'Yes', 'YES'):
-                print('------------------------------')
-                print(' ')
+                lanjut_interogasi = input("Lanjut interogasi (Y/N))?")
+            if lanjut_interogasi in (yes_answer):
+                print('------------------------------\n')
                 continue
-            elif lanjut_interogasi in ('n', 'N', 'no', 'No', 'NO'):
-                print('Interogasi telah selesai')
-                print('')
+            elif lanjut_interogasi in no_answer:
+                print('Interogasi telah selesai\n')
                 print('>>==========================<<')
                 print('||                     	    ||')
                 print('|| Ending the Interrogation ||')
@@ -106,7 +84,6 @@ while jawaban_interogasi in ('y', 'Y', 'yes', 'Yes', 'YES'):
                 print('>>==========================<<')
                 jawaban_interogasi = 'N'
                 break
-            else:
-                print('')
-                print('input tidak valid')
-                continue
+            while lanjut_interogasi not in (no_answer, yes_answer):
+                print('\ninput tidak valid')
+                lanjut_interogasi = input("Lanjut interogasi (Y/N))?")
